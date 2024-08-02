9e56071bc94838341dfe5e59f22789ef2bdefec7
kopeclu4 <kopeclu4@fit.cvut.cz>
2024-08-02 17:41:33 +0200

    debugging errors

--- src/Header.js
+++ src/Header.js
@@ -28,6 +28,23 @@ const Header = () => {
       const lon = position.coords.longitude;
       const lat = position.coords.latitude;
       navigate(`/city/${lon}/${lat}`);
+    },
+    (error) => {
+      console.error(error);
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          alert("User denied the request for Geolocation.");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          alert("Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          alert("The request to get user location timed out.");
+          break;
+        case error.UNKNOWN_ERROR:
+          alert("An unknown error occurred.");
+          break;
+      }
     })
   }
 
