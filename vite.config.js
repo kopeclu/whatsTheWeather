bf51e5608a876a2fddf1f71027470e73678dadde
Lukáš Kopečný <lukas.kope@seznam.cz>
2026-04-02 16:40:20 +0200

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     port: 3000, // Keeps the same port you are used to with CRA
   },
