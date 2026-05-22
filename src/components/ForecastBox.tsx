70c0db8daccff6a96391c29f243fcc2c3a502182
Lukáš Kopečný <lukas.kope@seznam.cz>
2026-05-22 14:57:58 +0200

    redesign forecast box

--- src/components/ForecastBox.tsx
+++ src/components/ForecastBox.tsx
@@ -15,19 +15,18 @@ const ForecastBox = ({data, timezone, metric}: ForecastBoxProps) => {
   const weatherStatus = data.weather[0].main;
 
   return (
-    <div className="min-w-75 h-full flex flex-col items-center bg-white/50 backdrop-blur-md shadow-md rounded-3xl p-5 border border-white/60 hover:bg-white/60 transition-colors">
+    <div className="min-w-75 h-full flex flex-col items-center bg-white/50 shadow-md rounded-3xl p-5 gap-7">
       
-      <h5 className="text-xl font-bold text-gray-800 mb-3">
+      <h5 className="text-xl font-bold text-gray-800">
         {timestampHours}
       </h5>
       
-      <div className="w-20 h-20 flex items-center justify-center bg-linear-to-br from-blue-100/50 to-white/50 rounded-full shadow-inner border border-white/60 mb-2">
-        <img src={iconURL} alt={weatherStatus} className="w-16 object-contain drop-shadow-sm" />
+      <div className="w-20 h-20 flex flex-col items-center justify-center">
+        <img src={iconURL} alt={weatherStatus} className="w-16 object-contain" />
+        <h3 className="text-lg font-semibold text-gray-700 -mt-2">
+          {weatherStatus}
+        </h3>
       </div>
-      
-      <h5 className="text-sm font-semibold text-gray-500 uppercase tracking-wider mb-5">
-        {weatherStatus}
-      </h5>
       
       <div className="grid grid-cols-2 gap-2 w-full grow">
         {STATS_TO_DISPLAY.map((el) => (
